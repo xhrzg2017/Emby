9f860908fb77ca88b6375321ed85ad85e22a979a
xhrzg2017 <34803905+xhrzg2017@users.noreply.github.com>
2022-07-19 18:03:03 +0800

    Update update.py

--- update.py
+++ update.py
@@ -56,7 +56,7 @@ elif(judge[0] == "jibaketa"):
     EP = re.findall("\d+", name1[1])[0]
     seanson1 = name[length - 2]
     seanson = re.findall("\d+", seanson1)[0]
-    os.rename(text, newpath + 'S' + seanson + 'E' + EP + '-TVB' +'.' + houzhui)
+    os.rename(text, newpath + 'S' + seanson + 'E' + EP + '.' + houzhui)
     nn = name[length - 3]
     nn = nn.encode("ISO-8859-1").decode('utf-8')    
 elif(judge[0] == "Lilith-Raws"):
@@ -67,7 +67,14 @@ elif(judge[0] == "Lilith-Raws"):
     os.rename(text, newpath + 'S' + seanson + 'E' + EP + '.' + houzhui)
     nn = name[length - 3]
     nn = nn.encode("ISO-8859-1").decode('utf-8')    
-    
+elif(judge[0] == "LoliHouse"):
+    name1 = text.split(' - ')
+    EP = re.findall("\d+", name1[1])[0]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + 'S' + seanson + 'E' + EP + '.' + houzhui)
+    nn = name[length - 3]
+    nn = nn.encode("ISO-8859-1").decode('utf-8')   
 time.sleep(5)
 os.system('rclone copy --drive-chunk-size 64M /media/Emby_Temp/ emby:/动漫追更/2022-7/ &')  # 这里只是例子，请根据自己的实际路径更改，/media/Emby_Temp/是存放所有动漫的文件夹，比如间谍过家家的目录是：/media/Emby_Temp/间谍过家家
 time.sleep(90)
