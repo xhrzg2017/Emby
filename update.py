659828d68511b88273e0fad822ad7ccc531357f4
xhrzg2017 <34803905+xhrzg2017@users.noreply.github.com>
2022-07-24 14:21:30 +0800

    Update update.py

--- update.py
+++ update.py
@@ -91,7 +91,22 @@ elif(judge[0] == "Nekomoe kissaten"):
     os.rename(text, newpath + SN +' - S' + seanson + 'E' + EP + '.' + houzhui)
     nn = name[length - 3]
     nn = nn.encode("ISO-8859-1").decode('utf-8')
-
+elif(judge[0] == "Skymoon-Raws"):
+    EP = judge[2]
+    SN = judge[1]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + SN +' - S' + seanson + 'E' + EP + '.' + houzhui)
+    nn = name[length - 3]
+    nn = nn.encode("ISO-8859-1").decode('utf-8')
+elif(judge[0] == "OPFansMaplesnow"):
+    EP = judge[2]
+    SN = judge[1]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + SN +' - S' + seanson + 'E' + EP + '.' + houzhui)
+    nn = name[length - 3]
+    nn = nn.encode("ISO-8859-1").decode('utf-8')
 time.sleep(5)
 os.system('rclone copy --drive-chunk-size 64M /media/Emby_Temp/ emby:/动漫追更/2022-7/ &')  # 这里只是例子，请根据自己的实际路径更改，/media/Emby_Temp/是存放所有动漫的文件夹，比如间谍过家家的目录是：/media/Emby_Temp/间谍过家家
 time.sleep(90)
