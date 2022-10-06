264a8bbf770c969b8e6176b4aaf53b6e16032303
xhrzg2017 <34803905+xhrzg2017@users.noreply.github.com>
2022-10-06 23:09:29 +0800

    Update update.py

--- update.py
+++ update.py
@@ -13,128 +13,124 @@ houzhui = name[length - 1][-3:]
 newpath = text[:-length_end]
 filename = text[-length_end:]
 
-
 zhengze = re.compile(r'\[(.+?)\]', re.S)
-try:
-    judge = re.findall(zhengze, filename)
-    test = judge[0]
 
-else:
-    if (judge[0] == "NC-Raws"):
-        name1 = text.split(' - ')
-        EP = re.findall("\d+", name1[1])[0]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + 'S' + seanson + 'E' + EP + '.' + houzhui)
+judge = re.findall(zhengze, filename)
+
+if (judge[0] == "NC-Raws"):
+    name1 = text.split(' - ')
+    EP = re.findall("\d+", name1[1])[0]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + 'S' + seanson + 'E' + EP + '.' + houzhui)
 
 
-    elif (judge[0] == "NaN-Raws"):
-        EP = judge[1]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + 'S' + seanson + 'E' + EP + '.' + houzhui)
+elif (judge[0] == "NaN-Raws"):
+    EP = judge[1]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + 'S' + seanson + 'E' + EP + '.' + houzhui)
 
 
-    elif (judge[0] == "ANi"):
-        name1 = text.split(' - ')
-        EP = re.findall("\d+", name1[1])[0]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + 'S' + seanson + 'E' + EP + '.' + houzhui)
+elif (judge[0] == "ANi"):
+    name1 = text.split(' - ')
+    EP = re.findall("\d+", name1[1])[0]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + 'S' + seanson + 'E' + EP + '.' + houzhui)
 
 
 
 
 
-    elif (judge[0] == "jibaketa"):
-        name1 = text.split(' - ')
-        EP = re.findall("\d+", name1[1])[0]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + 'S' + seanson + 'E' + EP + ' - 粤语.' + houzhui)
+elif (judge[0] == "jibaketa"):
+    name1 = text.split(' - ')
+    EP = re.findall("\d+", name1[1])[0]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + 'S' + seanson + 'E' + EP + ' - 粤语.' + houzhui)
 
 
-    elif (judge[0] == "Lilith-Raws"):
-        name1 = text.split(' - ')
-        EP = re.findall("\d+", name1[1])[0]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + 'S' + seanson + 'E' + EP + '.' + houzhui)
+elif (judge[0] == "Lilith-Raws"):
+    name1 = text.split(' - ')
+    EP = re.findall("\d+", name1[1])[0]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + 'S' + seanson + 'E' + EP + '.' + houzhui)
 
-    elif (judge[0] == "LoliHouse"):
-        name1 = text.split(' - ')
-        EP = re.findall("\d+", name1[1])[0]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + 'S' + seanson + 'E' + EP + '.' + houzhui)
+elif (judge[0] == "LoliHouse"):
+    name1 = text.split(' - ')
+    EP = re.findall("\d+", name1[1])[0]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + 'S' + seanson + 'E' + EP + '.' + houzhui)
 
-    elif (judge[0] == "WMSUB"):
-        EP = judge[2]
-        SN = judge[1]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
-
-
-    elif (judge[0] == "SBSUB"):
-        EP = judge[2]
-        SN = judge[1]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
-
-    elif (judge[0] == "CoolComic404"):
-        EP = judge[2]
-        SN = judge[1]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
+elif (judge[0] == "WMSUB"):
+    EP = judge[2]
+    SN = judge[1]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
 
 
-    elif (judge[0] == "Nekomoe kissaten"):
-        EP = judge[2]
-        SN = judge[1]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
+elif (judge[0] == "SBSUB"):
+    EP = judge[2]
+    SN = judge[1]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
+
+elif (judge[0] == "CoolComic404"):
+    EP = judge[2]
+    SN = judge[1]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
 
 
-    elif (judge[0] == "Skymoon-Raws"):
-        EP = judge[2]
-        SN = judge[1]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
+elif (judge[0] == "Nekomoe kissaten"):
+    EP = judge[2]
+    SN = judge[1]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
 
 
-    elif (judge[0] == "OPFansMaplesnow"):
-        EP = judge[2]
-        SN = judge[1]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
+elif (judge[0] == "Skymoon-Raws"):
+    EP = judge[2]
+    SN = judge[1]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
 
 
-    elif (judge[0] == "DMG"):
-        EP = judge[2]
-        SN = judge[1]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
+elif (judge[0] == "OPFansMaplesnow"):
+    EP = judge[2]
+    SN = judge[1]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
 
 
-    elif (judge[0] == "XKsub"):
-        EP = judge[2]
-        SN = judge[1]
-        seanson1 = name[length - 2]
-        seanson = re.findall("\d+", seanson1)[0]
-        os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
+elif (judge[0] == "DMG"):
+    EP = judge[2]
+    SN = judge[1]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
 
+
+elif (judge[0] == "XKsub"):
+    EP = judge[2]
+    SN = judge[1]
+    seanson1 = name[length - 2]
+    seanson = re.findall("\d+", seanson1)[0]
+    os.rename(text, newpath + SN + ' - S' + seanson + 'E' + EP + '.' + houzhui)
 
 time.sleep(5)
-upload='rclone copy --drive-chunk-size 64M /media/Emby_Temp/ gd1:/emby/02-动漫追更/2022-10/ &'
-sh=upload.encode("utf-8").decode("latin1")
+upload = 'rclone copy --drive-chunk-size 64M /media/Emby_Temp/ gd1:/emby/02-动漫追更/2022-10/ &'
+sh = upload.encode("utf-8").decode("latin1")
 os.system(sh)
 print('Upload successful')
-time.sleep(5)
+time.sleep(90)
 shutil.rmtree(newpath[:-9])
